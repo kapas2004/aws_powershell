78994b22acad277eb115041a9844939caba68503
kapas2004 <kapas2004@users.noreply.github.com>
2020-05-24 22:09:26 +0200

    Update tag_root_volume_aws.ps1

--- powershell/tag_root_volume_aws.ps1
+++ powershell/tag_root_volume_aws.ps1
@@ -8,20 +8,22 @@ param (
 )
 #Get all instances in the region set
 $instances = (aws ec2 describe-instances --filter Name=platform,Values=windows --region $region --query "Reservations[].Instances[].InstanceId" --output text).split()
-
+#count how many instances are
 $count = 1
+#write in sda1Tags.txt file
 $temp1 ="Total Number of Instances contain /dev/sda1 volume: " + $instances.Count
 $temp1  > sda1Tags.txt
 echo $temp1
-foreach($instance in $instances) {
-    
-    #get the volumeId and Instance Tag:Name
+foreach($instance in $instances) {    
+    #get the tagged Instance Name
     $TagValue = aws ec2 describe-instances --instance-ids $instance --filters Name=block-device-mapping.device-name,Values=/dev/sda1 --query "Reservations[].Instances[].[Tags[?Key=='Name'].Value]" --output text
+    #get the volumeId from volume description
     $sda1VolId = aws ec2 describe-volumes --filters Name=attachment.instance-id,Values=$instance Name=attachment.device,Values=/dev/sda1 --query "Volumes[].Attachments[].VolumeId" --output text    
+    #get the current tag if exists
     $currentTag = aws ec2 describe-volumes --filters Name=attachment.instance-id,Values=$instance Name=attachment.device,Values=/dev/sda1 --query "Volumes[].Tags[?Key=='Name'].Value" --output text
-    
-    #put Instance Tag:Name in a separate variable
+    #add (C) to Instance Tag:Name at the end
     $TagValueC = $TagValue + " (C)"
+    #check if the tag with the same value exists
     if($TagValueC -eq $currentTag){
         echo "$count. InstanceId: $instance, tag with Key:'Name',Value:'$TagValueC' exists, volumeId: $sda1VolId"
         "$count InstanceId: $instance, tag with Key:'Name',Value:'$TagValueC' exists, volumeId: $sda1VolId" >> sda1Tags.txt
@@ -32,5 +34,8 @@ foreach($instance in $instances) {
         "$count. InstanceId: $instance, tag with Key:'Name',Value:'$TagValueC' created, volumeId: $sda1VolId" >> sda1Tags.txt
         echo "$count. InstanceId: $instance, tag with Key:'Name',Value:'$TagValueC' created, volumeId: $sda1VolId"
     }
+    #increment the count
     $count++
 }
+
+
