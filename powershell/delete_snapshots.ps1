0e0de5f141e7a599934d9987062376fdc068a728
kapas2004 <kapas2004@users.noreply.github.com>
2020-05-26 12:27:30 +0200

    Update delete_snapshots.ps1

--- powershell/delete_snapshots.ps1
+++ powershell/delete_snapshots.ps1
@@ -1,10 +1,12 @@
 #
-#.\snapshotDelete.ps1 "region" "older than days" "confirm delete" "tagKey" tagValue" "*"_or_"InstanceId"
+#.\snapshotDelete.ps1 "region" ""owner-id "older than days" "confirm delete" "tagKey" tagValue" "*"_or_"InstanceId"
 #
 [CmdLetBinding()]
 param (
     [string][Parameter(Position=0)]
     $region = "eu-west-2",
+    [string][Parameter(Position=5)]
+    $ownerID = "xxxxxxxxxxxx",
     [int][Parameter(Position=1)]
     $startTime = -1,
     [boolean][Parameter(Position=2)]
@@ -12,7 +14,7 @@ param (
     [string][Parameter(Position=3)]
     $tagkey = "Description",
     [string][Parameter(Position=4)]
-    $tagvalue = "SQL Server",
+    $tagvalue = "ALL Server",
     [string][Parameter(Position=5)]
     $instancemanual = "*"
 )
@@ -21,7 +23,7 @@ param (
 $getDate = (Get-Date (Get-Date).AddDays($startTime) -Format "yyyy-MM-dd")
 if ($instancemanual -eq "*"){
 	#Collect Volumes
-	$instances = aws ec2 describe-instances --region eu-west-2 --filters "Name=tag:$tagkey, Values=$tagvalue" --query "Reservations[*].Instances[*].[InstanceId]" --output text
+	$instances = aws ec2 describe-instances --region eu-west-2 --query "Reservations[*].Instances[*].[InstanceId]" --output text
 	#$instances = aws ec2 describe-instances --region $region --filters "Name=instance-id, Values=$instancemanual" --query "Reservations[*].Instances[*].[InstanceId]" --output text
 	$fileName = "Deleted-$tagvalue"+"s AllSnapshots"
 }
@@ -45,6 +47,9 @@ catch
     exit 1
 }
 
+$snapCount = ((aws ec2 describe-snapshots --owner-id $ownerID --region 'eu-west-2' --output json | ConvertFrom-Json).Snapshots.SnapshotId).Count
+echo "Number of snapshots in $region to be messed with up: $snapCount"
+
 foreach($instance in $instances) {
     $instanceName = aws ec2 describe-instances --output text --filters "Name=instance-id, Values=$instance" --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value | [0]]'
     $volumes = (aws ec2 describe-instances --instance-ids $instance --query Reservations[*].Instances[*].BlockDeviceMappings[*].Ebs.VolumeId --output text).Split("	")
@@ -55,10 +60,11 @@ foreach($instance in $instances) {
         #$getDate = (Get-Date (Get-Date).AddDays($startTime) -Format "yyyy-MM-dd")
 
         #check id snapshots exests for delete
-        $snapexists=(aws ec2 describe-snapshots --filters "Name=volume-id, Values=$volume" --query "Snapshots[?StartTime<='$getDate'].SnapshotId" --output text)
+        $snapexists=(aws ec2 describe-snapshots --owner-id $ownerID --filters "Name=volume-id, Values=$volume" --query "Snapshots[].SnapshotId" --output text)
 
         if($snapexists -ne $null){
-            $snapshots_to_delete=(aws ec2 describe-snapshots --filters "Name=volume-id, Values=$volume" --query "Snapshots[?StartTime<='$getDate'].SnapshotId" --output text).Split("	")
+            $snapshots_to_delete=(aws ec2 describe-snapshots --owner-id $ownerID --filters "Name=volume-id, Values=$volume" --query "Snapshots[].SnapshotId" --output text).Split("	")
+            
     
             #failsafe
             if($delete -eq $true){
@@ -79,25 +85,28 @@ foreach($instance in $instances) {
                 
                 foreach($snap in $snapshots_to_delete){
                     try{
-                        $dateCreated = Get-date (aws ec2 describe-snapshots --filters --snapshot-ids $snap --query Snapshots[*].StartTime --output text) -Format "yyyy-MM-dd"
+                        #get the snapshots start time
+                        $dateCreated = Get-date (aws ec2 describe-snapshots --owner-id $ownerID --filters --snapshot-ids $snap --query Snapshots[*].StartTime --output text) -Format "yyyy-MM-dd"
                         
-                        aws ec2 delete-snapshot --snapshot-id $snap 2>err.txt
+                        if($getDate -lt $dateCreated) {
+                            aws ec2 delete-snapshot --snapshot-id $snap 2>err.txt
 
-                        if((Get-Content -Path .\err.txt | Where-Object {$_ -match 'InvalidSnapshot.InUse'}).Length -ne 0){
-                            Write-Host (Get-Content -Path .\err.txt | Where-Object {$_ -match 'InvalidSnapshot.InUse'})
-                            Add-Content -Path .\$fileName.csv -Value "$instanceName,$instance,$volume,$dateCreated,$snap, InUse"
-                        } 
+                            if((Get-Content -Path .\err.txt | Where-Object {$_ -match 'InvalidSnapshot.InUse'}).Length -ne 0){
+                                Write-Host (Get-Content -Path .\err.txt | Where-Object {$_ -match 'InvalidSnapshot.InUse'})
+                                Add-Content -Path .\$fileName.csv -Value "$instanceName,$instance,$volume,$dateCreated,$snap, InUse"
+                            } 
                         
-                        elseif ((Get-Content -Path .\err.txt | Where-Object {$_ -match 'InvalidSnapshot.NotFound'}).Length -ne 0)
-                        {
-                            Write-Host  (Get-Content -Path .\err.txt | Where-Object {$_ -match 'InvalidSnapshot.NotFound'})
-                            Add-Content -Path .\$fileName.csv -Value "$instanceName,$instance,$volume,$dateCreated,$snap, Not Found"
-                        }
-                        else{
-                            echo "Snapshot for $instanceName, id: $instance, vol: $volume, created on: $dateCreated is deleted: $snap"
-                            Add-Content -Path .\$fileName.csv -Value "$instanceName,$instance,$volume,$dateCreated,$snap, $True"
-                            $count++
+                            elseif ((Get-Content -Path .\err.txt | Where-Object {$_ -match 'InvalidSnapshot.NotFound'}).Length -ne 0)
+                            {
+                                Write-Host  (Get-Content -Path .\err.txt | Where-Object {$_ -match 'InvalidSnapshot.NotFound'})
+                                Add-Content -Path .\$fileName.csv -Value "$instanceName,$instance,$volume,$dateCreated,$snap, Not Found"
                             }
+                            else{
+                                echo "Snapshot for $instanceName, id: $instance, vol: $volume, created on: $dateCreated is deleted: $snap"
+                                Add-Content -Path .\$fileName.csv -Value "$instanceName,$instance,$volume,$dateCreated,$snap, $True"
+                                $count++
+                                }
+                        }
                     }
                     catch{
                         echo "Error"
@@ -126,4 +135,3 @@ foreach($instance in $instances) {
 
 echo ""
 echo "Done."
-
