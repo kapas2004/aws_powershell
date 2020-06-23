94b87ab6472e805f7d219744123d2d622c12ab2b
kapas2004 <kapas2004@gmail.com>
2020-06-23 17:15:41 +0200

    Explain added at the top of the code

--- powershell/aws-delete-acm-unused-certificates.ps1
+++ powershell/aws-delete-acm-unused-certificates.ps1
@@ -1,22 +1,33 @@
-#$regions = (aws ec2 describe-regions --query "Regions[].RegionName" --output text).Split('	')
-$regions = ("eu-west-2 eu-west-1 ap-southeast-1 ap-southeast-2 eu-central-1 us-east-1").Split()
+#
+# Created by kapas2004
+#
+# Delete Unused AWS ACM Certificate by iterating all regions
+# using mixed powershell and aws cli
+# 
+#Usage
+#.\aws-delete-acm-unused-certificates.ps1
+#
+# TODO Need fixing if region does not have ACM Certificates hide error and write on console no ACM found
+#
+
+$regions = (aws ec2 describe-regions --query "Regions[].RegionName" --output text).Split('	')
 foreach($region in $regions) {
     $inUseCount = 0
     $removedCount = 0
-    echo "Checking Region $region "
+    Write-Output "Checking Region $region "
     $certs = (aws acm list-certificates --certificate-statuses --certificate-statuses --region $region --query CertificateSummaryList[].CertificateArn --output text).Split('	')
     foreach($cert in $certs) {
         $certInUse = aws acm describe-certificate --region $region --certificate-arn $cert --query "Certificate.InUseBy[]"
         if($certInUse.Count -le 1){
-            echo "remove $cert by uncomment the line below"
+            Write-Output "remove $cert by uncomment the line below"
             #aws acm delete-certificate --certificate-arn $cert
             $removedCount += 1
         }
         else {
-            echo "Certificate in use by:" $certInUse
+            Write-Output "Certificate in use by:" $certInUse
             $inUseCount += 1
         }
     }
-    echo "Total Certs removed $removedCount in the $region region"
-    echo "Total Certs in use $inUseCount in the $region region"
+    Write-Output "Total Certs removed $removedCount in the $region region"
+    Write-Output "Total Certs in use $inUseCount in the $region region"
 }
