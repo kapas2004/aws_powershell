5925ba6f826cc9c4645d3388949f15385ee6b4a8
kapas2004 <kapas2004@gmail.com>
2020-06-24 23:23:16 +0200

    modified csv outputfile name to hold the region too

--- powershell/count_stopped_instance_type_for_cost_optimization.ps1
+++ powershell/count_stopped_instance_type_for_cost_optimization.ps1
@@ -1,5 +1,5 @@
 ﻿#
-# List Instance types not running for cost optimization
+# List of manually added general instance types not running for cost optimization
 # using mixed powershell and aws cli
 # 
 #Usage
@@ -7,6 +7,7 @@
 #
 
 $state = "stopped"
+$region = "eu-west-2"
 $instance_types = "m4", "t3a", "t3a", "t2", "r5a", "r5", "r4", "m5a", "m5", "m4", "c5d", "c5"
 
 if($state -eq "stopped"){
@@ -20,7 +21,7 @@ if($state -eq "running"){
 foreach($instance_type in $instance_types) {
     $count = `
     aws ec2 describe-instances `
-        --region eu-west-2 `
+        --region $region `
         --filters "Name=instance-state-name,Values=$state" "Name=instance-type,Values=$instance_type.*" `
         --query "Reservations[].Instances[].[InstanceId] | length(@)" `
         --output text
@@ -28,6 +29,6 @@ foreach($instance_type in $instance_types) {
     if($count -ne 0){
         
         Write-Output "$instance_type, $count"
-        "$instance_type, $count" >> "$state.csv"
+        "$instance_type, $count" >> "$region-$state.csv"
     }
 }
