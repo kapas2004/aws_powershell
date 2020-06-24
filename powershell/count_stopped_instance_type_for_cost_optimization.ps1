49900f2fb00facda7f93d50053278cf3c028cf24
kapas2004 <kapas2004@gmail.com>
2020-06-24 23:17:44 +0200

    count_stopped_instance_type_for_cost_optimization.ps1

---
+++ powershell/count_stopped_instance_type_for_cost_optimization.ps1
@@ -0,0 +1,33 @@
+﻿#
+# List Instance types not running for cost optimization
+# using mixed powershell and aws cli
+# 
+#Usage
+#.\count_stopped_instance_type_for_cost_optimization.ps1
+#
+
+$state = "stopped"
+$instance_types = "m4", "t3a", "t3a", "t2", "r5a", "r5", "r4", "m5a", "m5", "m4", "c5d", "c5"
+
+if($state -eq "stopped"){
+    Write-Output "Instance types stopped:"
+}
+
+if($state -eq "running"){
+    Write-Output "Instance types running:"
+}
+
+foreach($instance_type in $instance_types) {
+    $count = `
+    aws ec2 describe-instances `
+        --region eu-west-2 `
+        --filters "Name=instance-state-name,Values=$state" "Name=instance-type,Values=$instance_type.*" `
+        --query "Reservations[].Instances[].[InstanceId] | length(@)" `
+        --output text
+
+    if($count -ne 0){
+        
+        Write-Output "$instance_type, $count"
+        "$instance_type, $count" >> "$state.csv"
+    }
+}
